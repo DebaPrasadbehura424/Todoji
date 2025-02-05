16fc8e9a45cff73cb26bce219c351f2785c3a2e9
DebaPrasadbehura424 <DebaPrasadbehura424@gmail.com>
2025-02-06 04:53:32 +0530

    body

--- todo/src/main/java/com/todo/controller/TaskController.java
+++ todo/src/main/java/com/todo/controller/TaskController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 // import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +28,6 @@ public class TaskController {
 
     @GetMapping
     public String getTasks(Model model) {
-        System.out.println("");
         List<Task> tasks = taskService.getAllTasks();
         model.addAttribute("tasks", tasks);
         return "tasks";
@@ -36,6 +36,18 @@ public class TaskController {
     @PostMapping
     public String makeTasks(@RequestParam String title) {
         taskService.makeTasks(title);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/del")
+    public String delTasks(@PathVariable Long id) {
+        taskService.delTask(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/toggle")
+    public String toggleTask(@PathVariable Long id) {
+        taskService.toggleTask(id);
         return "redirect:/";
     }
 
--- todo/src/main/java/com/todo/service/TaskService.java
+++ todo/src/main/java/com/todo/service/TaskService.java
@@ -1,6 +1,7 @@
 package com.todo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 // import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,6 +27,21 @@ public class TaskService {
         Task task = new Task();
         task.setTitle(title);
         task.setCompleted(false);
+        taskRepository.save(task);
+    }
+
+    public void delTask(Long id) {
+        taskRepository.deleteById(id);
+
+    }
+
+    public void toggleTask(Long id) {
+        Optional<Task> taskOptional = taskRepository.findById(id);
+        if (taskOptional == null) {
+            throw new IllegalArgumentException("task id is null");
+        }
+        Task task = taskOptional.get();
+        task.setCompleted(!task.isCompleted());
         taskRepository.save(task);
     }
 
